2cbcba040cd4023e2247d6be2bea126a7a67d33b
m-hayashi13 <m-hayashi@advanced-media.co.jp>
2023-12-26 09:52:11 +0900

    update example/lib/main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,98 +14,42 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.grey[200],
         appBar: AppBar(),
-        body: HomeView(),
+        body: Column(
+          children: [
+            WeeklyCalendar(
+              calendarStyle: const CalendarStyle(
+                locale: "en_US",
+                padding: EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+                margin: EdgeInsets.all(14),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(14))),
+                headerDateTextColor: Colors.white,
+                headerDateTextAlign: Alignment.center,
+                isShowHeaderDateText: false,
+                footerDateTextColor: Colors.white,
+                footerDateTextAlign: Alignment.center,
+                isShowFooterDateText: false,
+                selectedCircleColor: Colors.white,
+                todaySelectedCircleColor: Colors.greenAccent,
+                dayTextColor: Colors.white,
+                todayDayTextColor: Colors.greenAccent,
+                selectedDayTextColor: Colors.black,
+                weekendDayTextColor: Colors.grey,
+                dayOfWeekTextColor: Colors.white,
+                weekendDayOfWeekTextColor: Colors.grey,
+              ),
+              isAutoSelect: true,
+              onChangedSelectedDate: (date) {
+                debugPrint("onChangedSelectedDate: $date");
+              },
+              onChangedPage: (date, state) {
+                debugPrint("onChangedPage: $date ${state.name}");
+              },
+            ),
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        calendar0(),
-        calendar1(),
-        calendar2(),
-        //Spacer(),
-      ],
-    );
-  }
-
-  Widget calendar0() {
-    return WeeklyCalendar(
-      calendarStyle: const CalendarStyle(
-        padding: EdgeInsets.symmetric(vertical: 14, horizontal: 20),
-        margin: EdgeInsets.all(14),
-        decoration: BoxDecoration(),
-      ),
-      isAutoSelect: true,
-      onChangedSelectedDate: (date) {
-        debugPrint("onChangedSelectedDate: $date");
-      },
-      onChangedPage: (date, state) {
-        debugPrint("onChangedPage: $date ${state.name}");
-      },
-    );
-  }
-
-  Widget calendar1() {
-    return WeeklyCalendar(
-      calendarStyle: const CalendarStyle(
-          locale: "en_US",
-          padding: EdgeInsets.symmetric(vertical: 14, horizontal: 20),
-          margin: EdgeInsets.all(14),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-          ),
-          selectedCircleColor: Colors.blueAccent,
-          todayDayTextColor: Colors.deepPurple,
-          todaySelectedCircleColor: Colors.deepPurple),
-      isAutoSelect: false,
-      onChangedSelectedDate: (date) {
-        debugPrint("onChangedSelectedDate: $date");
-      },
-      onChangedPage: (date, state) {
-        debugPrint("onChangedPage: $date ${state.name}");
-      },
-    );
-  }
-
-  Widget calendar2() {
-    return WeeklyCalendar(
-      calendarStyle: const CalendarStyle(
-        locale: "zh_CN",
-        padding: EdgeInsets.symmetric(vertical: 14, horizontal: 20),
-        margin: EdgeInsets.all(14),
-        decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.all(Radius.circular(14))),
-        headerDateTextColor: Colors.white,
-        headerDateTextAlign: Alignment.center,
-        isShowHeaderDateText: false,
-        footerDateTextColor: Colors.white,
-        footerDateTextAlign: Alignment.center,
-        isShowFooterDateText: false,
-        selectedCircleColor: Colors.white,
-        todaySelectedCircleColor: Colors.greenAccent,
-        dayTextColor: Colors.white,
-        todayDayTextColor: Colors.greenAccent,
-        selectedDayTextColor: Colors.black,
-        weekendDayTextColor: Colors.grey,
-        dayOfWeekTextColor: Colors.white,
-        weekendDayOfWeekTextColor: Colors.grey,
-      ),
-      isAutoSelect: true,
-      onChangedSelectedDate: (date) {
-        debugPrint("onChangedSelectedDate: $date");
-      },
-      onChangedPage: (date, state) {
-        debugPrint("onChangedPage: $date ${state.name}");
-      },
     );
   }
 }
